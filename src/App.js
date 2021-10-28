75046c7c4e79134cb74c803881bdad7e03af6b31
Steve Bateman <steve@steves-mac-mini.lan>
2021-10-28 16:33:09 -0600

    base template and routing -noComplete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import './App.css';
-import TemLogo from './components/TemLogo';
-import HomeLogo from "./HomeLogo";
-import TemNavbar from './components/TemNavbar';
-import TemFooter from './components/TemFooter';
-import Home from './components/Home';
-import Blog from './components/Blog';
-import Authors from './components/Authors';
-import Contact from './components/Contact';
-import Login from './components/Login';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Container from "../node_modules/react-bootstrap/Container";
+import Col from "../node_modules/react-bootstrap/Col";
+import Row from "../node_modules/react-bootstrap/Row";
+import TemLogo from "./components/TemLogo";
+import HomeLogo from "./components/HomeLogo";
+import { Route, Switch } from "react-router-dom";
+import TemNavbar from "./components/TemNavbar";
+import TemFooter from "./components/TemFooter";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import Authors from "./components/Authors";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <TemLogo />
-      <HomeLogo />
-      <TemNavbar />
-      <Home />
-      <Blog />
-      <Authors />
-      <Contact />
-      <Login />
+      <Row>
+        <Col lg="3">
+          <Switch>
+            <Route exact path="/">
+              <HomeLogo />
+            </Route>
+            <Route path="/">
+              <TemLogo />
+            </Route>
+          </Switch>
+        </Col>
+        <Col>
+          <TemNavbar />
+        </Col>
+      </Row>
+      <div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/authors">
+          <Authors />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+      </div>
       <TemFooter />
     </div>
   );
--- src/components/TemFooter.js
+++ src/components/TemFooter.js
@@ -1,10 +1,43 @@
+import Navbar from "../../node_modules/react-bootstrap/Navbar";
+import Nav from "../../node_modules/react-bootstrap/Nav";
+import Row from "../../node_modules/react-bootstrap/Row";
+import Col from "../../node_modules/react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
-function TemFooter () {
-    return (
-        <div class = "tem-footer">
-
-        </div>
-    )
+function TemFooter() {
+  return (
+    <div class="tem-footer">
+      <Navbar bg="dark" variant="dark" fixed="bottom">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Row>
+              <Col className="d-flex">
+                <Navbar.Text>Copyright 2021 Steve Bateman</Navbar.Text>
+              </Col>
+              <Col >
+                <Nav className="d-flex">
+                  <Link className="nav-link" to="/">
+                    home
+                  </Link>
+                  <Link className="nav-link" to="/blog">
+                    blog
+                  </Link>
+                  <Link className="nav-link" to="/authors">
+                    authors
+                  </Link>
+                  <Link className="nav-link" to="/contact">
+                    contact
+                  </Link>
+                  <Link className="nav-link" to="/login">
+                    login
+                  </Link>
+                </Nav>
+              </Col>
+            </Row>
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default TemFooter
+export default TemFooter;
