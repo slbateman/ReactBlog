56b956a90729b72975641474098d31742abfd596
Steve Bateman <steve@steves-mac-mini.lan>
2021-11-01 09:23:21 -0600

    added images to articles

--- src/data/ArticleList.js
+++ src/data/ArticleList.js
@@ -1,8 +1,17 @@
-import Shoggoth from "../images/shoggoth.jpeg";
+import image01 from "../images/image01.jpeg";
+import image02 from "../images/image02.png";
+import image03 from "../images/image03.jpeg";
+import image04 from "../images/image04.webp";
+import image05 from "../images/image05.jpeg";
+import image06 from "../images/image06.jpeg";
+import image07 from "../images/image07.webp";
+import image08 from "../images/image08.jpg";
+import image09 from "../images/image09.jpeg";
+import image10 from "../images/image10.jpeg";
 
 const ArticleList = [
     {
-        image: Shoggoth,
+        image: image01,
         imageAlt: "Scary Monster 1",
         title: "Monsters Around The World",
         author: "Steve Bateman",
@@ -10,75 +19,75 @@ const ArticleList = [
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image02,
         imageAlt: "Scary Monster 2",
         title: "Test Title2",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Alfred Hitchcock",
+        date: "2021-10-21",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image03,
         imageAlt: "Scary Monster 3",
         title: "Test Title3",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "HR Giger",
+        date: "2021-10-15",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image04,
         imageAlt: "Scary Monster 4",
         title: "Test Title4",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Vincent Price",
+        date: "2021-09-29",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image05,
         imageAlt: "Scary Monster 5",
         title: "Test Title5",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "HR Giger",
+        date: "2021-09-13",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image06,
         imageAlt: "Scary Monster 6",
         title: "Test Title6",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Steve Bateman",
+        date: "2021-08-24",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image07,
         imageAlt: "Scary Monster 7",
         title: "Test Title7",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Vincent Price",
+        date: "2021-08-07",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image08,
         imageAlt: "Scary Monster 8",
         title: "Test Title8",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Alfred Hitchcock",
+        date: "2021-07-18",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image09,
         imageAlt: "Scary Monster 9",
         title: "Test Title9",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "HR Giger",
+        date: "2021-07-12",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     },
     {
-        image: "",
+        image: image10,
         imageAlt: "Scary Monster 10",
         title: "Test Title10",
-        author: "Test Author",
-        date: "2021-10-27",
+        author: "Steve Bateman",
+        date: "2021-07-01",
         body: "This is the body. The best body that has ever been written. Everyone has told me so. ",
     }
 ]
